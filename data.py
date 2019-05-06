f6a6e855efdcbd5f3a8d6fd46e05c2abc9566e5b
megaelius <abad.elias00@gmail.com>
2019-05-06 13:40:55 +0200

    familiarizándonos con el grafo

--- data.py
+++ data.py
@@ -1,10 +1,12 @@
 import pandas
-import networkx
+import networkx as nx
 from jutge import read
 
 def main():
-    x = read(int)
-    print(x)
+    G = nx.DiGraph()
+    G.add_edge(1,3)
+    print(G.number_of_nodes())
+    print(list(G.nodes))
 main()
 
 # prueba
