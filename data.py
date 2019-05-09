2fbcf0ee450f0659b919e3b90095889e48329a9e
Naxel100 <ferrando936@gmail.com>
2019-05-09 17:55:24 +0200

    k

--- data.py
+++ data.py
@@ -19,15 +19,17 @@ def Graph(dist = 1000):
         G.add_node(st)
         for dt in bicing.itertuples():
             coord2 = (dt.lat, dt.lon)
-            if(st != dt and haversine(coord1, coord2) <= dist): G.add_edge(st, dt)
+            weight = haversine((st.lat, st.lon), (dt.lat, dt.lon))
+            if(st != dt and weight <= dist): G.add_edge(st, dt, weight = weight)
     print("Graph created!")
     return G
 
-def Plotgraph(G,name):
+
+def Plotgraph(G, name):
     try:
         m_bcn = stm.StaticMap(1000, 1000)
         for node in G.nodes:
-            marker = stm.CircleMarker((node.lon, node.lat) , 'red', 3 )#esto es el tamaño del punto
+            marker = stm.CircleMarker((node.lon, node.lat) , 'red', 3 ) #esto es el tamaño del punto
             m_bcn.add_marker(marker)
 
         for edge in G.edges:
@@ -40,14 +42,18 @@ def Plotgraph(G,name):
     except:
         print("This is not a graph!") # Revisar en el futuro el try / excepts
 
+
 def Components(G):
     print("This Graph has", G.number_connected_components(), "connected components")
+
 
 def Nodes(G):
     print("This Graph has", G.number_of_nodes(), "nodes")
 
+
 def Edges(G):
     print("This Graph has", G.number_of_edges(), "edges")
+
 
 def addressesTOcoordinates(addresses):
     try:
@@ -59,36 +65,59 @@ def addressesTOcoordinates(addresses):
     except:
         return None
 
+
 def Route(G, addresses):
     coord1, coord2 = addressesTOcoordinates(addresses)
-    print(coord1)
-    print(coord2)
+    start = Pandas(lat = coord1[0] , lon = coord1[1])
+    finish = Pandas(lat = coord2[0] , lon = coord2[1])
+    G.add_node(start)
+    G.add_node(finish)
     Gc = nx.complement(G)
+    G.remove_node(start)
+    G.remove_node(finish)
     #este bucle modifica los pesos
     for edge in Gc.edges:
-        weight=haversine((edge[0].lat, edge[0].lon), (edge[1].lat, edge[1].lon))
-        Gc.add_edge(edge[0],edge[1],weight = 10/4*weight)
-    '''
-    ATENCION, para añadir los nodes coord1 y coord2,
+        weight = haversine((edge[0].lat, edge[0].lon), (edge[1].lat, edge[1].lon))
+        Gc.add_edge(edge[0], edge[1], weight = 10/4 * weight)
+        #Gc[edge[0]][edge[1]]['weight'] = weight
+
+    '''ATENCION, para añadir los nodes coord1 y coord2,
     los he creado de manera que se pueda acceder a sus coordenadas de la forma:
     noseque.lat noseque.lon, para eso he creao un tipo namedtuple, que es lo mismo
     que te devuelve itertuples() y por lo tanto el mismo formato que tienen las
     estaciones del DataFrame.
     '''
-    start = Pandas(lat=coord1[0] , lon=coord1[1])
-    finish = Pandas(lat=coord2[0] , lon=coord2[1])
-    Gc.add_node(start)
-    Gc.add_node(finish)
     #este añade las arestas que unen start y finish con los demás
-    for node in Gc.nodes:
-        weight1=haversine((node.lat, node.lon),(start.lat, start.lon))
-        weight2=haversine((node.lat, node.lon),(finish.lat, finish.lon))
-        Gc.add_edge(node,start,weight = 10/4*weight1)
-        Gc.add_edge(node,finish,weight = 10/4*weight2)
-    Plotgraph(Gc,'complement.png')
+    '''for node in Gc.nodes:
+        weight1 = haversine((node.lat, node.lon),(start.lat, start.lon))
+        weight2 = haversine((node.lat, node.lon),(finish.lat, finish.lon))
+        Gc.add_edge(node, start, weight = 10/4 * weight1)
+        Gc.add_edge(node, finish, weight = 10/4 * weight2)'''
+    #Plotgraph(Gc,'complement.png')
     Gc = nx.compose(G, Gc)
-    Plotgraph(Gc,'union.png')
-    print(nx.dijkstra_path(Gc,start,finish))
+    print(nx.dijkstra_path(Gc, start, finish))
+
+    for edge in Shortest_Path:
+        print(edge)
+        print(edge[0])
+        print(edge[1])
+        print(edge[0].lat)
+
+    m_bcn = stm.StaticMap(1000, 1000)
+    for edge in Shortest_Path:
+        if Gc[edge[0]][edge[1]]['weight'] == haversine((edge[0].lat, edge[0][1].lon), (edge[1].lat, edge[1].lon)):
+            line = stm.Line(((edge[0].lon, edge[0].lat),(edge[1].lon, edge[1].lat)), 'blue', 0)
+        else:
+            line = stm.Line(((edge[0].lon, edge[0].lat),(edge[1].lon, edge[1].lat)), 'orange', 0)
+
+        marker1 = stm.CircleMarker((edge[0].lon, edge[0].lat) , 'red', 3 ) #esto es el tamaño del punto
+        marker2 = stm.CircleMarker((edge[1].lon, edge[1].lat) , 'red', 3 ) #esto es el tamaño del punto
+        m_bcn.add_marker(marker, marker2)
+        m_bcn.add_line(line)
+
+    image = m_bcn.render()
+    image.save("Shortest_Path.png")
+    print("Image done!")
 
     # hacer dykstra con Gc
     # ¿Crear un nuevo grafo con el trayecto dado para poder printearlo?
