5237a897f1c0357af9d8415050ec4fb932fd3fcd
megaelius <abad.elias00@gmail.com>
2019-05-09 11:00:21 +0200

    components

--- mierda.py
+++ mierda.py
@@ -24,7 +24,7 @@ def Create_Graph(dist = 1000):
     return G
 
 def Components(G):
-    return nx.number_connected_components(G)
+    print("This Graph has",nx.number_connected_components(G),"connected components")
 
 def Paint_Graph(G):
     try:
