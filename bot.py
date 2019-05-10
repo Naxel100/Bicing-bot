6a825d6f5ddd973906f21cbc5b5cd22a6f942e6f
megaelius <abad.elias00@gmail.com>
2019-05-10 13:39:55 +0200

    bot

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ import data.py as data
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Hola! Soc un bot bàsic.")
+
 
 TOKEN = open('token.txt').read().strip()
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
-
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Hola! Soc un bot bàsic.")
 
 dispatcher.add_handler(CommandHandler('start', start))
 
