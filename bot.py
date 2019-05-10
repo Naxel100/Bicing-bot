5feb5b448dd8108bc7b7c77fb97dbc56710bbb17
megaelius <abad.elias00@gmail.com>
2019-05-10 13:38:45 +0200

    bot

--- bot.py
+++ bot.py
@@ -8,9 +8,9 @@ TOKEN = open('token.txt').read().strip()
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Hola! Soc un bot bàsic.")
+
 dispatcher.add_handler(CommandHandler('start', start))
 
 updater.start_polling()
-
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Hola! Soc un bot bàsic.")
