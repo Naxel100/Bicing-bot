12d535b6400a59c2df29ca645014b7be6c06de01
Naxel100 <ferrando936@gmail.com>
2019-05-10 14:15:49 +0200

    shit

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 import telegram
-import data.py as data
+#import data.py as data
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
+def start(bot, update):
+    print(bot)
+    print(update)
+    botname = bot.username
+    username = update.message.chat.username
+    fullname = update.message.chat.first_name + ' ' + update.message.chat.last_name
+    missatge = "Tu ets en %s (%s) i jo soc el %s." % (fullname, username, botname)
+    bot.send_message(chat_id=update.message.chat_id, text=missatge)
+
 
 TOKEN = open('token.txt').read().strip()
+
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start))
 
 updater.start_polling()
-
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Hola! Soc un bot bàsic.")
